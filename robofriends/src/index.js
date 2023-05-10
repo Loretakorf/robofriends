fd51ad351c07c06d4cc701a0869f2feaed355db4
loretakorf <96435648+Loretakorf@users.noreply.github.com>
2023-05-10 16:15:58 +0300

    helloWorld

---
+++ robofriends/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Hello from "./Hello";
+import reportWebVitals from "./reportWebVitals";
+import 'tachyons'
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Hello  greeting={'Hello React Ninja'}/>
+  </React.StrictMode>
+);
+
+reportWebVitals();
